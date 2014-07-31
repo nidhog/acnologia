568f1329b145a0dac65dac4501ed32318cde8aeb
nidhogg <ismail.elouafiq@telecom-bretagne.eu>
2014-07-31 01:44:40 +0100

    first breath

---
+++ model.py
@@ -0,0 +1,22 @@
+"""
+Will contain the learning models
+
+"""
+
+import os
+import sys
+import numpy
+import theano
+import cPickle
+import theano.tensor as T
+
+def LogisticRegressionModel():
+    pass
+
+def HiddenLayer():
+    pass
+
+def Perceptron():
+    pass
+
+
